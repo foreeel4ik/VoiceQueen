1cacd0400b8af433b0eb8774d3a7b47ab1bc8029
Farid Dadashov <2194551@student.edu.az>
2025-11-30 01:15:41 +0400

    Add animated transitions and neon interactions

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 using VoiceQueen.Views;
 
 namespace VoiceQueen
@@ -11,6 +13,8 @@ namespace VoiceQueen
             InitializeComponent();
             Loaded += OnLoaded;
             AppState.Instance.PropertyChanged += OnAppStateChanged;
+            RootFrame.Navigated += RootFrame_Navigated;
+            RootFrame.Navigating += RootFrame_Navigating;
         }
 
         private void OnLoaded(object sender, RoutedEventArgs e)
@@ -42,6 +46,55 @@ namespace VoiceQueen
                     RootFrame.Navigate(new LoginPage());
                 }
             }
+        }
+
+        private void RootFrame_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            if (RootFrame.Content is FrameworkElement element)
+            {
+                BeginFadeSlide(element, 1, 0.94, 0, 14, 180);
+            }
+        }
+
+        private void RootFrame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (e.Content is FrameworkElement element)
+            {
+                element.Opacity = 0;
+                element.RenderTransformOrigin = new Point(0.5, 0.5);
+                element.RenderTransform = new TranslateTransform(0, 16);
+
+                element.Loaded += (_, _) => BeginFadeSlide(element, 0, 1, 16, 0, 240);
+            }
+        }
+
+        private static void BeginFadeSlide(FrameworkElement element, double fromOpacity, double toOpacity, double fromY, double toY, double durationMs)
+        {
+            var transform = element.RenderTransform as TranslateTransform ?? new TranslateTransform();
+            element.RenderTransform = transform;
+
+            var storyboard = new Storyboard
+            {
+                Duration = TimeSpan.FromMilliseconds(durationMs)
+            };
+
+            var fade = new DoubleAnimation(fromOpacity, toOpacity, storyboard.Duration)
+            {
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut }
+            };
+            Storyboard.SetTarget(fade, element);
+            Storyboard.SetTargetProperty(fade, new PropertyPath(UIElement.OpacityProperty));
+
+            var slide = new DoubleAnimation(fromY, toY, storyboard.Duration)
+            {
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut }
+            };
+            Storyboard.SetTarget(slide, transform);
+            Storyboard.SetTargetProperty(slide, new PropertyPath(TranslateTransform.YProperty));
+
+            storyboard.Children.Add(fade);
+            storyboard.Children.Add(slide);
+            storyboard.Begin();
         }
     }
 }
